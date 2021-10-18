b978e55e715d5ff1f437f38452955dd8668a5352
Neerajkumar205 <91404790+Neerajkumar205@users.noreply.github.com>
2021-10-17 22:21:40 -0700

    Create Question 26.c

---
+++ Question 26.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a1,a2,a3;
+    scanf("%d",&a1);
+    scanf("%d",&a2);
+    a3=(180-(a1+a2));
+    printf("Third angle of the triangle: %d \n",a3);
+return 0;
+}
